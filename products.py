ee16ceb18301eb3dc64cb2d403ec247c141ab639
YongKang Sun <kangocoeuc@hotmail.com>
2023-09-05 16:07:57 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,26 @@
+products = []
+while True:
+	name = input('请输入商品名称: ')
+	if name == 'q':
+		break
+	price = input('请输入商品价格: ')
+	#p = []
+	#p.append(name)
+	#p.append(price)
+		#p = [name, price]
+	products.append([name, price])
+print(products)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
