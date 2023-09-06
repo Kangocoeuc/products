86f11b3767b3615390d012f592d9c94b5ff474db
YongKang Sun <kangocoeuc@hotmail.com>
2023-09-06 19:24:39 +0800

    new verison

--- products.py
+++ products.py
@@ -1,4 +1,13 @@
 products = []
+#读取档案
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		name, price = line.strip().split(',')  #以,为切割标准
+		products.append([name, price])
+
+print(products)
+
+
 while True:
 	name = input('请输入商品名称: ')
 	if name == 'q':
@@ -16,7 +25,6 @@ with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,价格\n')
 	for p in products:
 		f.write(p[0] + ',' +p[1] + '\n')
-
 
 
 
